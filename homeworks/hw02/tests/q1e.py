9bc656684091eea248a773c0f7eb94dad4090d6c
Ishaan Malhi <ishaan.malhi@berkeley.edu>
2020-01-20 20:08:09 -0800

    feat: add hw02 files
    feat: fix endpoint and tests
    
    chore: folder rearrange and better naming for datahub pull
    
    feat: additional test case fixes
    
    chore: clear notebook output

---
+++ homeworks/hw02/tests/q1e.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> sum_a
+          2532
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
