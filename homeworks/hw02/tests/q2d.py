6fee2bffc40d185c455ead7029edc04064ab79f0
lilliandong <31335786+lilliandong@users.noreply.github.com>
2020-01-29 18:33:29 -0800

    update q2d.py
    (was previously same as q2c.py)

--- homeworks/hw02/tests/q2d.py
+++ homeworks/hw02/tests/q2d.py
@@ -6,8 +6,8 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> occurences
-          4
+          >>> b
+          ['1', 'am', 'so', 'exc1ted', 'about', 'Data-X.', '1t', '1s', '1mportant', 'to', 'be', 'able', 'to', 'work', 'w1th', 'data.']
           """,
           'hidden': False,
           'locked': False
