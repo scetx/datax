1b28b16c4bf8560d35c10510aa51bfc538cb70c6
Lillian Dong <lilliandong@Lillians-MacBook-Air.local>
2020-02-27 09:47:28 -0800

    add some tests

---
+++ homeworks/hw06/tests/q1e.py
@@ -0,0 +1,15 @@
+test = {   'name': 'q1e',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> sum(train_df.isnull().sum()) == 0 \n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                {   'code': ">>> sum(test_df.isnull().sum()) == 0 \n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}
+                                   ],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
