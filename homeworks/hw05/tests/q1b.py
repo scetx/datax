e39d4a7b75b78232493eaa081a854a476515a423
Joshua Sanchez <joshuarafael@berkeley.edu>
2020-06-05 01:37:28 -0700

    remove duplicated directory

--- homeworks/hw05/tests/q1b.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q1b',
-    'points': 1,
-    'suites': [   {   'cases': [   {   'code': '>>> any_nulls == False\nTrue',
-                                       'hidden': False,
-                                       'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
