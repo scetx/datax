585fbfd2cf08120a0cee46e7fcd464c78cb6c7d3
Yu-Hung Liao <henryl1152@gmail.com>
2021-04-18 17:32:31 -0700

    HW5_Flask: fix blog.index starter code

--- dataxHWSp2021/HW5_Flask/student/flaskr/blog.py
+++ dataxHWSp2021/HW5_Flask/student/flaskr/blog.py
@@ -12,8 +12,7 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = []
-    db.execute(
+    posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username, anonym'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
